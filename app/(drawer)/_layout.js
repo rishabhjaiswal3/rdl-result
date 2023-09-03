637aac8500b925eb70dade271ad28b9be38eb2bb
Rishabh Jaiswal <rj838486@gmail.com>
2023-09-03 22:05:33 +0530

    added step3

--- app/(drawer)/_layout.js
+++ app/(drawer)/_layout.js
@@ -1,12 +1,88 @@
 import { Drawer } from "expo-router/drawer";
+import Header from "../component/Header";
+import { View, Text, Pressable, Image, TouchableOpacity } from "react-native";
+export function CustomDrawerContent({ navigation, items }) {
+  const data = [
+    {
+      id: 1,
+      title: "RDL Delhi",
+    },
+    { id: 2, title: "RDL Mumbai" },
+    {
+      id: 3,
+      title: "RDL Kolkata",
+    },
+    { id: 4, title: "RDL Haryana" },
+    {
+      id: 5,
+      title: "Faridabad",
+    },
+    { id: 6, title: "Ghaziabad" },
+    {
+      id: 7,
+      title: "Gali",
+    },
+    { id: 8, title: "Deshawar" },
+  ];
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ padding: 10, height: 200, backgroundColor: "#007201" }}>
+        <View style={{ marginTop: 20, marginLeft: 4 }}>
+          <Pressable onPress={() => navigation.navigate("home")}>
+            <Image
+              source={require("../../assets/icon1.png")}
+              style={{
+                height: 120,
+                width: 90,
+                marginBottom: 4,
+                marginLeft: 2,
+              }}
+            />
+          </Pressable>
+          <Text
+            style={{ color: "white" }}
+            onPress={() => navigation.navigate("home")}
+          >
+            RDL Results
+          </Text>
+          <Text
+            style={{ color: "white" }}
+            onPress={() => navigation.navigate("home")}
+          >
+            RDL@gmail.com
+          </Text>
+        </View>
+      </View>
+
+      {data.map((temp) => {
+        return (
+          <TouchableOpacity
+            style={{ padding: 14, marginVertical: 2 }}
+            onPress={() =>
+              navigation.navigate("city", {
+                searchData: temp,
+              })
+            }
+            key={temp?.id}
+          >
+            <Text style={{ fontWeight: "bold" }}>{temp.title}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
 export default function Layout() {
   return (
-    <Drawer>
+    <Drawer drawerContent={(props) => <CustomDrawerContent {...props} />}>
       <Drawer.Screen
         name="home"
         options={{
           drawerLabel: "Home",
           title: "Home",
+          header: () => <Header />,
         }}
       ></Drawer.Screen>
       <Drawer.Screen
@@ -14,6 +90,7 @@ export default function Layout() {
         options={{
           drawerLabel: "City",
           title: "City",
+          header: () => <Header />,
         }}
       ></Drawer.Screen>
     </Drawer>
---
+++ app/component/Header.js
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from "react";
+import {
+  TouchableOpacity,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  Share,
+} from "react-native";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+// import { getShareUrl } from "../service/city";
+
+const Header = () => {
+  const navigation = useNavigation();
+  const [currentURL, setCurrentURL] = useState("");
+
+  const getShareUrl = () => {
+    try {
+      const apiKey = "https://rdl-result.com/api";
+      axios.get(apiKey + "/getSharingData").then((response) => {
+        console.log("api 2 :- ", response);
+        const data = response?.data;
+        setCurrentURL(`${data.message} ${data.url}`);
+        // setCurrentURL();
+      });
+    } catch (e) {
+      console.log("error is", e);
+    }
+  };
+
+  useEffect(() => {
+    getShareUrl();
+  }, []);
+
+  const onShare = async () => {
+    try {
+      await Share.share({
+        message: currentURL,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <View style={styles.headerContainer}>
+      <View style={styles.headerContent}>
+        <Ionicons
+          onPress={() => navigation.openDrawer()}
+          name="menu"
+          size={32}
+          color="white"
+        />
+        <Text style={styles.headerTitle}>RDL Results</Text>
+        <Image
+          source={require("../../assets/icon1.png")}
+          style={styles.headerIcon}
+        />
+        <TouchableOpacity onPress={() => onShare()} style={styles.shareButton}>
+          <Text style={styles.shareButtonText}>Share</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    padding: 10,
+    backgroundColor: "#007201",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5, // For Android
+  },
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  headerTitle: {
+    color: "white",
+    fontSize: 20,
+    marginLeft: 0,
+  },
+  headerIcon: {
+    width: 40,
+    height: 40,
+  },
+  shareButton: {
+    padding: 5,
+  },
+  shareButtonText: {
+    color: "white",
+  },
+});
+
+export default Header;
